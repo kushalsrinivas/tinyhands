1c8f4922ef2dae22b3c2eea089a2f23fcbd9a764
kushalsrinivas <kushal.s.2005@gmail.com>
2023-08-12 10:21:24 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,108 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="h-full w-full">
+        <div className="h-32 w-screen bg flex text-center align-middle justify-center font-bold p-8 text-black">
+          <h1 className="text-5xl">Tiny Hands</h1>
+        </div>
+        <div className="hero">
+          <div className="hero-container">
+            <div className="text-white text-center flex flex-col gap-5 h-full justify-center align-middle">
+              <h1 className="text-7xl">Paint Your Way To Fun</h1>
+              <p className="text-white text-2xl w-1/2 text-center mx-auto">
+                Let your kids unleash their creativity with our coaster and
+                glass bottle painting workshop for ages 5-13.
+              </p>
+              <div>
+                <button className="  py-2 px-3  text-black bg rounded-md">
+                  Register Now !
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-auto w-screen p-10 text-center bg-yellow-300">
+          <div className="flex flex-col gap-10">
+            <h1 className="text-7xl text-black">About Us</h1>
+            <p className="text-2xl text-justify w-3/4 text-black mx-auto">
+              At tiny hands, we specialize in creating a fun and engaging
+              environment for kids aged 5 to 13 years. Located in Bengaluru, KA,
+              our workshops focus on coaster painting and glass bottle painting,
+              allowing children to explore their creativity and develop new
+              skills. Our experienced instructors provide step-by-step guidance,
+              ensuring that each child feels supported and inspired throughout
+              the creative process. Join us at tiny hands and let your childs
+              imagination soar!
+            </p>
+          </div>
+        </div>
+        <div className="h-auto w-screen p-10 bg-prime text-black text-center">
+          <div className="flex flex-col">
+            <div>
+              <h1 className="text-7xl text-black">Our Services</h1>
+            </div>
+            <div className="flex flex-row">
+              <div className="card flex flex-col gap-5 p-5">
+                <div className="card-img "></div>
+                <div className="text-5xl">
+                  <h1>Glass Painting Workshop</h1>
+                </div>
+                <div className="text-sm">
+                  Let your child unleash their creativity with our coaster
+                  painting workshop, designed for kids between the ages of 5 and
+                  13. Our experienced instructors will guide them through the
+                  process, ensuring a fun and educational experience.
+                </div>
+                <button className="  py-2 px-3  text-black bg rounded-md">
+                  Register Now !
+                </button>
+              </div>
+
+              <div className="card flex flex-col gap-5 p-5">
+                <div className="card-img "></div>
+                <div className="text-5xl">
+                  <h1>Shoe Painting Workshop</h1>
+                </div>
+                <div className="text-sm">
+                  Looking for twice the fun? Sign up for our combo workshop and
+                  let your child experience the joy of both coaster and glass
+                  bottle painting. Suitable for kids aged 5 to 13, our workshop
+                  promises a unique and engaging learning experience unlike any
+                  other!
+                </div>
+                <button className="  py-2 px-3  text-black bg rounded-md">
+                  Register Now !
+                </button>
+              </div>
+
+              <div className="card flex flex-col gap-5 p-5">
+                <div className="card-img "></div>
+                <div className="text-5xl">
+                  <h1>Coaster Painting Workshop</h1>
+                </div>
+                <div className="text-sm">
+                  Watch your childs imagination come to life as they transform
+                  plain glass bottles into works of art! Our glass bottle
+                  painting workshop is perfect for kids aged 5 to 13. Our
+                  instructors will provide all necessary materials and guidance
+                  for a fun and productive session
+                </div>
+                <button className="  py-2 px-3  text-black bg rounded-md">
+                  Register Now !
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-32 w-screen bg flex text-center align-middle justify-center font-bold p-8 text-black">
+          <h1 className="text-5xl">Tiny Hands</h1>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
